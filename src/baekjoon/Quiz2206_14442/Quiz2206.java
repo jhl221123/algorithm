6ce6d62c1c28efd0636f287626204e34dcdd02b2
jhl221123 <jhl221123@naver.com>
2024-02-22 22:37:18 +0900

    백준 2206 벽 부수고 이동하기

---
+++ src/baekjoon/Quiz2206_14442/Quiz2206.java
@@ -0,0 +1,65 @@
+package baekjoon.Quiz2206_14442;
+
+import java.util.*;
+import java.io.*;
+public class Quiz2206 {
+	// 방문배열과 벽 부술 수 있는 횟수 같이 활용
+	static int N;
+	static int M;
+	static int[][] arr;
+	static int[][][] visit;
+	static int[] dy = {-1, 1, 0, 0};
+	static int[] dx = {0, 0, -1, 1};
+	static int min = Integer.MAX_VALUE;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		visit = new int[N][M][2];
+		for(int i=0; i<N; i++) {
+			String str = br.readLine();
+			for(int j=0; j<M; j++) {
+				arr[i][j] = str.charAt(j) - '0';
+			}
+		}
+		bfs();
+		if(visit[N-1][M-1][0] == 0 && visit[N-1][M-1][1] == 0) System.out.println(-1);
+		else if(visit[N-1][M-1][0] == 0) System.out.println(visit[N-1][M-1][1]);
+		else if(visit[N-1][M-1][1] == 0) System.out.println(visit[N-1][M-1][0]);
+		else System.out.println(Math.min(visit[N-1][M-1][0], visit[N-1][M-1][1]));
+	}
+	private static void bfs() {
+		ArrayDeque<Status> ad = new ArrayDeque<>();
+		ad.addLast(new Status(new int[] {0, 0}, 0));
+		visit[0][0][0] = 1;
+		while(!ad.isEmpty()) {
+			Status cs = ad.removeFirst();
+			int[] point = cs.point;
+			int breakCount = cs.breakCount;
+			int r = point[0];
+			int c = point[1];
+			for(int i=0; i<4; i++) {
+				int newBreakCount = breakCount;
+				int mr = r + dy[i];
+				int mc = c + dx[i];
+				if(mr < 0 || mr >= N || mc < 0 || mc >= M) continue;
+				if(arr[mr][mc] == 1 && breakCount==1) continue;
+				if(arr[mr][mc] == 1 && breakCount==0) newBreakCount++;
+				if(visit[mr][mc][newBreakCount] == 0) {
+					visit[mr][mc][newBreakCount] = visit[r][c][breakCount] + 1;
+					ad.addLast(new Status(new int[] {mr, mc}, newBreakCount));
+				}
+			}
+		}
+	}
+}
+class Status {
+	int[] point;
+	int breakCount;
+	public Status(int[] point, int breakCount) {
+		this.point = point;
+		this.breakCount = breakCount;
+	}
+}
